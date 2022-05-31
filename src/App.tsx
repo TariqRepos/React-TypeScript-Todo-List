1f6b21fe2b73a3705b13f1af1ec6e11b612d151a
unknown <tariqmuhanna14@gmail.com>
2022-05-30 20:02:11 -0500

    Project running

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import './App.css';
 
-let name: string;
-let age: number;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-
-let person: Object;
-
 const App = () => {
   return (
     <div>
