88bb001a6da2ec61c89aff2372d01dd083a20083
unknown <tariqmuhanna14@gmail.com>
2022-05-30 19:58:36 -0500

    PART 1: Setup project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+let name: string;
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+let person: Object;
+
+const App = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
+
